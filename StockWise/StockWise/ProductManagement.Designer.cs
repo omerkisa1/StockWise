1026c7f049cd0ba2030bbb53bc0e9338480c3ea0
omerkisa1 <omereis.5555@gmail.com>
2024-10-24 18:52:36 +0300

    product management page grid controls and database updated

--- StockWise/StockWise/ProductManagement.Designer.cs
+++ StockWise/StockWise/ProductManagement.Designer.cs
@@ -28,20 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.stockManagementPage1 = new StockWise.StockManagementPage();
+            this.mygridControl = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mygridControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // stockManagementPage1
+            // 
+            this.stockManagementPage1.Location = new System.Drawing.Point(0, 0);
+            this.stockManagementPage1.Name = "stockManagementPage1";
+            this.stockManagementPage1.Size = new System.Drawing.Size(800, 450);
+            this.stockManagementPage1.TabIndex = 0;
+            // 
+            // mygridControl
+            // 
+            this.mygridControl.Location = new System.Drawing.Point(0, 0);
+            this.mygridControl.MainView = this.gridView1;
+            this.mygridControl.Name = "mygridControl";
+            this.mygridControl.Size = new System.Drawing.Size(800, 450);
+            this.mygridControl.TabIndex = 1;
+            this.mygridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            this.mygridControl.Click += new System.EventHandler(this.gridControl1_Click_1);
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.mygridControl;
+            this.gridView1.Name = "gridView1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get Stocks";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ProductManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mygridControl);
+            this.Controls.Add(this.stockManagementPage1);
             this.Name = "ProductManagement";
-            this.Text = "ProductManagement";
+            this.Size = new System.Drawing.Size(800, 450);
             this.Load += new System.EventHandler(this.ProductManagement_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mygridControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private StockManagementPage stockManagementPage1;
+        private DevExpress.XtraGrid.GridControl mygridControl;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- StockWise/StockWise/ProductManagement.cs
+++ StockWise/StockWise/ProductManagement.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Grid;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace StockWise
@@ -15,11 +13,103 @@ namespace StockWise
         public ProductManagement()
         {
             InitializeComponent();
+            InitializeGridControl();  
+        }
+
+      
+        private void InitializeGridControl()
+        {
+        
+            GridView gridView = new GridView(mygridControl);
+
+            mygridControl.MainView = gridView;
+            mygridControl.Dock = DockStyle.Fill; 
+
+           
+            gridView.Columns.AddVisible("ProductName", "Ürün Adı");
+            gridView.Columns.AddVisible("Price", "Fiyat");
+            gridView.Columns.AddVisible("Stock", "Stok Durumu");
+            gridView.Columns.AddVisible("Category", "Kategori");
+        }
+
+        
+        private void LoadProductsFromDatabase()
+        {
+            var productList = GetProductListFromDatabase(); 
+            if (productList.Count > 0)
+            {
+                mygridControl.DataSource = productList;
+            }
+            else
+            {
+                MessageBox.Show("Veri çekilemedi veya veri yok.");
+            }
+        }
+
+
+        private List<Product> GetProductListFromDatabase()
+        {
+            try
+            {
+    
+                var client = new MongoClient("mongodb://localhost:27017");
+                var database = client.GetDatabase("users"); 
+                var collection = database.GetCollection<BsonDocument>("usersStocks"); 
+
+          
+                var documents = collection.Find(new BsonDocument()).ToList();
+
+           
+                List<Product> productList = new List<Product>();
+
+                foreach (var doc in documents)
+                {
+                    var product = new Product
+                    {
+                        ProductName = doc.Contains("productName") ? doc["productName"].AsString : "Bilinmiyor",
+                        Price = doc.Contains("price") ? doc["price"].ToDecimal() : 0,
+                        Stock = doc.Contains("stock") ? doc["stock"].ToInt32() : 0,
+                        Category = doc.Contains("category") ? doc["category"].AsString : "Bilinmiyor"
+                    };
+                    productList.Add(product);
+                }
+
+                return productList; 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ürünler yüklenirken bir hata oluştu: " + ex.Message);
+                return new List<Product>(); 
+            }
+        }
+
+
+        public class Product
+        {
+            public string ProductName { get; set; }
+            public decimal Price { get; set; }
+            public int Stock { get; set; }
+            public string Category { get; set; }
         }
 
         private void ProductManagement_Load(object sender, EventArgs e)
         {
+           
+        }
 
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+    
+        }
+        private void gridControl1_Click_1(object sender, EventArgs e)
+        {
+   
+        }
+
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            LoadProductsFromDatabase(); 
         }
     }
 }
